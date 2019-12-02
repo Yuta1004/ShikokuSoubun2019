9e80507b414ccde61bbbc684b07df665d091966b
NakagamiYuta <guguru0014@gmail.com>
2019-12-02 16:22:47 +0900

    [add] 盤面サイズについてのテストを追加

--- board/validator.py
+++ board/validator.py
@@ -7,6 +7,22 @@ def error(msg):
     exit(1)
 
 
+def size_check(board_json):
+    width = board_json["width"]
+    height = board_json["height"]
+    tiled = board_json["tiled"]
+    points = board_json["points"]
+
+    assert 10 <= width and width <= 20
+    assert 10 <= height and height <= 20
+    assert len(tiled) > 0
+    assert len(tiled) == height
+    assert len(tiled[0]) == width
+    assert len(points) > 0
+    assert len(points) == height
+    assert len(points[0]) == width
+
+
 def main():
     # 引数検証
     if len(sys.argv) != 2:
@@ -19,6 +35,9 @@ def main():
             board_json = json.loads(f.read())
     except FileNotFoundError:
         error("FileNotFound : "+sys.argv[1])
+
+    # テスト
+    size_check(board_json)
 
 
 if __name__ == '__main__':
